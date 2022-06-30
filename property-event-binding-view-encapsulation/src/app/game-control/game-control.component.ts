4fecfe64bf265a7ccb91af4fca549d37305146b0
sistiri <sistiri@yahoo.com>
2022-06-30 15:01:40 +0200

    Adding onStartGame() & onPauseGame() to GameControl

--- property-event-binding-view-encapsulation/src/app/game-control/game-control.component.ts
+++ property-event-binding-view-encapsulation/src/app/game-control/game-control.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+
 
 @Component({
-  selector: 'app-game-control',
-  templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  selector: "app-game-control",
+  templateUrl: "./game-control.component.html",
+  styleUrls: ["./game-control.component.css"],
 })
 export class GameControlComponent implements OnInit {
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1)
+      this.lastNumber++
+    }, 1000);
   }
 
+  onPauseGame() {
+    clearInterval(this.interval)
+  }
 }
